3f148ce538b022d88fc5b21a58e5e96b880bfbb5
Nguyen Van Nam <mrnamnguyen18@gmail.com>
2018-11-02 08:52:36 +0700

    thêm điều hướng user trong admin.php

--- admin.php
+++ admin.php
@@ -10,12 +10,36 @@
 </head>
 <body>
 	<?php 
-		session_start();
-		include 'config.php';
-		include 'application/model.php';
-		include 'application/controller.php';
+	session_start();
+	include 'config.php';
+	include 'application/model.php';
+	include 'application/controller.php';
 		//include 'controller/backend/controller_taikhoan.php';
-		include 'controller/backend/controller_doctor.php';
-	 ?>
+	if (isset($_SESSION["acc_username"]) == false) 
+	{
+		//hien thi MVC login
+		include "controller/backend/controller_login.php";
+	}else
+	{
+		//------------
+		//xac dinh controller truyen tu url de load trang
+		$controller = isset($_GET["controller"])&&$_GET["controller"]!=""?"controller_".$_GET["controller"].".php":"controller_home.php";
+
+		if ($_SESSION['acc_username'] == 'nguyennam') {
+			include "view/backend/view_layout_CanBo.php";
+		}
+		else if ($_SESSION["acc_username"] == 'vuthihue') {
+			include "view/backend/view_layout.php";
+		}
+		else if ($_SESSION["acc_username"] == 'caovanhoc') {
+			include "view/backend/view_layout.php";
+		}
+		else if ($_SESSION["acc_username"] == 'maivanhoc') {
+			include "view/backend/view_layout_Medical.php";
+		}
+
+		//------------
+	}
+	?>
 </body>
 </html>
